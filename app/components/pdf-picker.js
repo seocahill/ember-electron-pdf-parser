7be2b3a465e396e77374f112c6f71741f79e76d7
Seosamh Cahill <seo.cahill@gmail.com>
2017-07-16 00:39:25 +0200

    Added file chooser

--- app/components/pdf-picker.js
+++ app/components/pdf-picker.js
@@ -1,16 +1,23 @@
 import Ember from 'ember';
 
 const fs = requireNode('fs');
-const { ipcRenderer } = requireNode('electron');
+const { ipcRenderer, remote } = requireNode('electron');
+const { dialog } = remote;
+
 
 export default Ember.Component.extend({
   actions: {
+
     parsePdf() {
       ipcRenderer.send('parse-pdf');
       ipcRenderer.on('parse-pdf-done', function (e, pdfData) {
         fs.writeFile("electron-pdf-test.json", JSON.stringify(pdfData));
         alert("Success!")
       })
+    },
+    
+    pickFile() {
+      dialog.showOpenDialog({ properties: ['openFile', 'openDirectory', 'multiSelections'] });
     }
   }
 });
